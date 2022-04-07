d97b2a3acd10e851af7e4c9ec050a277d87d70c1
Алексей <oskolkov@bk.ru>
2022-04-07 16:40:55 +0300

    2.15. Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let getRandom = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandom(1, 100);
+
+let lengthString = (string, maxLength) {
+  if (maxLenght <= 140) { }
+} 
+
+lengthString (1, 140)
