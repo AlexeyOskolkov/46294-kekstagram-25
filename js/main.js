45af3c605e5e02c940a54822f3151297eff740dc
Алексей <oskolkov@bk.ru>
2022-04-07 20:15:02 +0300

    2.15. Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-let getRandom = function (min, max) {
+const getRandom = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 getRandom(1, 100);
 
-let calculateLengthString = function (text) {
-  let maxLength = 140;
-  let lengthString = text.length;
+const calculateLengthString = function (text) {
+  const maxLength = 140;
+  const lengthString = text.length;
   if (lengthString <= maxLength) {
     return lengthString;
   }
-}
+};
 
-console.log('Длина строки ' + calculateLengthString('Hello') + ' символов');
+calculateLengthString('Hello');
