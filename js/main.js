4ab0e262e693bfa344d362e370080c8575c525b8
Алексей <oskolkov@bk.ru>
2022-04-09 09:36:24 +0300

    2.15. Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -10,8 +10,10 @@ const calculateLengthString = function (text) {
   const maxLength = 140;
   const lengthString = text.length;
   if (lengthString <= maxLength) {
-    return lengthString;
+    return true;
+  } else {
+    return false;
   }
 };
 
-calculateLengthString('Hello');
+calculateLengthString('Hello! How are you?');
